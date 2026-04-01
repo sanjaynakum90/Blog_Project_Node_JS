7a093c932722ff472f4df82d966e0b18e7376010
sanjaynakum90 <sanjaynakum4545@gmail.com>
2026-04-01 12:48:42 +0530

    project complated

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,16 +1,32 @@
 import express from "express";
-import uploads from "../middleware/upload.js";
+
 import blogController from "../controller/blogController.js";
+import upload from "../middleware/upload.js";
 
 const router = express.Router();
 
 router.post(
   "/add",
-  uploads.fields([
+  upload.fields([
     { name: "blogImage", maxCount: 1 },
     { name: "blogVideo", maxCount: 1 },
   ]),
-  blogController.createBlog
+  blogController.createBlog,
 );
+
+router.get("/allBlogs", blogController.getAllBlog);
+
+router.get("/:id", blogController.getBlogById);
+
+router.patch(
+  "/update/:id",
+  upload.fields([
+    { name: "blogImage", maxCount: 1 },
+    { name: "blogVideo", maxCount: 1 },
+  ]),
+  blogController.updateBlog,
+);
+
+router.delete("/delete/:id", blogController.deleteBlog);
 
 export default router;
